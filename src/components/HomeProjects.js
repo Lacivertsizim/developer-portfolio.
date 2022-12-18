1eaa585d381a6abcaced196903b3a8ebf84c3f1f
yusuf <yusufay1157@gmail.com>
2022-12-19 00:04:47 +0300

    first commit

---
+++ src/components/HomeProjects.js
@@ -0,0 +1,52 @@
+import { Container, Typography } from "@mui/material";
+import Grid from '@mui/material/Grid';
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+
+
+const projetcList = [
+  {
+    id: 1,
+    name: "Blogify",
+    info: "Blogify Info ",
+    Link: "https://github.com/Lacivertsizim/blogify",
+  },
+  {
+    id: 2,
+    name: "Exchange App",
+    info: "Exchange App Info",
+    Link: "https://github.com/Lacivertsizim/My-todo-list-react",
+  },
+  {
+    id: 3,
+    name: "Movie App",
+    info: "Movie App Info",
+    Link: "https://github.com/Lacivertsizim/weather-app",
+  },
+  {
+    id: 4,
+    name: "Todo App",
+    info: "Todo App",
+    Link: "https://github.com/Lacivertsizim/my-trivia-quiz-app",
+  },
+];
+
+const HomeProjects = () => {
+  return (
+    <Container maxWidth="lg" sx={{ p: 2 }}>
+      <Typography variant="h4" component="h2" sx={{my:2}}>
+        My Projects
+      </Typography>
+      <Grid container rowSpacing={1}>
+        {projetcList?.map((project) => (
+        <Grid item xs={12} md={6}>
+          <ProjectCard project={project} />
+        </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomeProjects;
